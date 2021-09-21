698a00245fa276e63440abe1108fb67e0d2e9687
Silvia García Sánchez <30872921+Garcia091@users.noreply.github.com>
2021-09-21 11:33:06 -0500

    Agregar JS

---
+++ script/data.js
@@ -0,0 +1,24 @@
+export const burger = 
+[
+    {
+        "id": 1,
+        "name": "Pollo",
+        "image": "/images/Chicken.png"
+    },
+    {
+        "id": 2,
+        "name": "Criolla",
+        "image": "/images/Criolla.png"
+    },
+    {
+        "id": 3,
+        "name": "Patagonia",
+        "image": "/images/patagonia.png"
+    },
+    {
+        "id": 4,
+        "name": "Porteña",
+        "image": "/images/Porteña.png"
+    }
+    
+]
---
+++ script/script.js
@@ -0,0 +1,33 @@
+import { burger } from './data.js'
+
+const card = document.querySelector('#cards')
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadBurger(burger)
+})
+
+function loadBurger(burger) {
+    card.innerHTML = ''
+
+    burger.forEach((data) => {
+        const items = document.createElement('div')
+        const { burguer, image } = data
+        items.innerHTML = `
+            <div class="col d-flex justify-content-center mb-4">
+                <div class="card shadow mb-1 bg-dark rounded text-white " style="width: 16rem;">
+                    <h5 class="card-title text-center pt-2">${name}</h5>
+                    <img src=${image} class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text text-white-50 description">Some quick example text to build on the card
+                            title and make up the bulk of the card's content.</p>
+                        <h5 class="text-primary">Precio: <span class="precio">20.000</span></h5>
+                        <div class="d-grid gap-2">
+                            <button class="btn btn-primary button" id="btnClick">Añadir a Carrito</button>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+        `
+        card.appendChild(items)
+    });
+}
